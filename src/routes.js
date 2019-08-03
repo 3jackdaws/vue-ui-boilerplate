8c80430fb9c8861b6c5bb01507c7e66a6598880d
Ian Murphy <3jackdaws@gmail.com>
2019-08-03 10:54:03 -0700

    Iniotial commit

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import Home from './components/pages/Home.vue';
+import Login from './components/pages/Login.vue';
+
+
+const routes = [
+    { path: '/', component: Home },
+    { path: '/login', component: Login }
+]
+
+
+export default routes
